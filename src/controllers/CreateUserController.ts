926e7f1940e9930a2003c4a417e79ea0d3e29e78
Edimar Pires <edimarfilho971@gmail.com>
2023-05-18 09:48:16 -0300

    Atualizando o migrations

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,19 +1,37 @@
 import {Response , Request } from "express"
 import { database } from "../database";
+const { hash } = require("bcrypt")
 
 
 export class CreateUserController {
     async handle(request: Request, response: Response) {
-        const {name, email } = request.body;
+        const { name, email, password } = request.body;
 
+        //Cripitografando senha
+        const hashedPassword = await hash(password, 10)
         //Criando o usuario
         const user = await database.user.create({
             data: {
                 name,
-                email
+                email,
+                password: hashedPassword,
             }
         })
-
         response.json(user)
     }
-}
+
+//     //Lidando com atualização de dados
+//     async update(request: Request, response: Response) {
+//         const { name, email, password, old_password } = request.body;
+//         const { id } = request.params;
+
+//         const database = await sqliteConnection();
+//         const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+//         user.name = name ?? user.name;
+//         user.email = email ?? user.email;
+    
+
+//     }
+ }
+
